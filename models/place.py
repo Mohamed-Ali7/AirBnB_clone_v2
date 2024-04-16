5d14a0e641d07c3c772721f144ea176f19377188
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-17 01:50:39 +0200

    Updating models/place.py, models/review.py, and models/user.py files

--- models/place.py
+++ models/place.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.review import Review
+import models
 from sqlalchemy import Column, String, Float, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
@@ -18,3 +21,19 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+
+    @property
+    def reviews(self):
+        """
+        Represents the FileStorage relationship between Place and Review and
+        returns the list of Review instances with place_id equals
+        to the current Place.id
+        """
+
+        reviews_objects = models.storage.all(Review)
+        place_reviews = []
+
+        for value in reviews_objects.values():
+            if value["place_id"] == self.id:
+                place_reviews.append(Review(**value))
