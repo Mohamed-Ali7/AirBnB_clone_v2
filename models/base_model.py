3bd32f311f752a85bd23419b0ef9737490a7262b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-17 00:55:22 +0200

    Updating models/base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -65,8 +65,9 @@ class BaseModel():
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
 
-        if hasattr(dictionary, "_sa_instance_state"):
-            del dictionary["_sa_instance_state "]
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
+        
         return dictionary
 
     def delete(self):
