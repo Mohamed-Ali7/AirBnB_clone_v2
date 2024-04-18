c801ec7fc35ec8520c8dc0b192c3ce6db7ef5c6d
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-18 07:43:37 +0200

    Updating models/__init__.py and console.py and Adding tests/test_console.py file

--- models/__init__.py
+++ models/__init__.py
@@ -3,15 +3,13 @@
 import os
 
 
-engine = os.getenv("HBNB_TYPE_STORAGE")
-
-if engine == "db":
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
 
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
 
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
