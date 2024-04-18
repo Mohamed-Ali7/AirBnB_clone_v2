644d96fd2ab436589fa3e960e4a7ba13c2655a7c
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-18 08:17:35 +0200

    Updating test_console.py file

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,15 +3,11 @@
 """
 import MySQLdb
 import os
-import sqlalchemy
 import unittest
 from io import StringIO
 from unittest.mock import patch
-
 from console import HBNBCommand
 from models import storage
-from models.base_model import BaseModel
-from models.user import User
 
 
 class TestHBNBCommand(unittest.TestCase):
