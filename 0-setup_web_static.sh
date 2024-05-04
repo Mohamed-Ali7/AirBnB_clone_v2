9b97a66bcbd5b36a83012767f19acb31bd198f02
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 13:10:30 +0300

    Making some updates for testing purposes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 # script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+apt-get update
+apt-get -y install nginx
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Hello World!" > /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+new="\n\
+\tlocation /hbnb_static {\n\
+\t\talias /data/web_static/current/;\n\
+\t}
+"
+sed -i "/listen \[::\]:80 default_server;/a\ $new" /etc/nginx/sites-available/default
 
-sudo service nginx restart
+ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+
+service nginx restart
