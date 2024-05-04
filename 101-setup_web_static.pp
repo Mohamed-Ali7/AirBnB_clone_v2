b3bc99fa52492ce38b2c8ca52e6ebe5927b8c9e7
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 10:33:32 +0300

    Updating 101-setup_web_static.pp file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,37 +2,45 @@
 
 exec { 'update':
   command => '/usr/bin/apt-get update',
-}
--> package { 'nginx':
-ensure          => installed,
-provider        => 'apt',
-install_options => ['-y'],
 } ->
+
+package { 'nginx':
+  ensure          => installed,
+  provider        => 'apt',
+  install_options => ['-y'],
+} ->
+
 exec {'create_test_directory':
-  command  => 'mkdir -p /data/web_static/releases/test/',
-  provider => 'shell',
+  command => 'mkdir -p /data/web_static/releases/test/',
+  path    => '/usr/bin/:/usr/local/bin/:/bin/',
 } ->
+
 exec { 'create_shared_directory':
-command  => 'mkdir -p /data/web_static/shared/',
-provider => 'shell',
+  command => 'mkdir -p /data/web_static/shared/',
+  path    => '/usr/bin/:/usr/local/bin/:/bin/',
 } ->
+
 file {'/data/web_static/releases/test/index.html':
-content => 'Hello World!',
+  content => 'Hello World!',
 } ->
+
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test',
 } ->
-exec { 'change owner':
-command  => 'chown -hR ubuntu:ubuntu /data/',
-provider => 'shell',
+
+exec { 'change_owner':
+  command => 'chown -hR mohamed:mohamed /data/',
+  path    => '/usr/bin/:/usr/local/bin/:/bin/',
 } ->
+
 file_line { 'add_alias':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen \[::\]:80 default_server;',
   line   => "\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}",
 } ->
+
 service { 'nginx':
   ensure => 'running',
 }
