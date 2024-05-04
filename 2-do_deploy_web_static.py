350ab6c4c92b066633ae30708ecba4c7f1c18248
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 12:29:57 +0300

    Making some updates for testing purposes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['54.208.120.231', '54.89.61.73']
 
 
 def do_deploy(archive_path):
