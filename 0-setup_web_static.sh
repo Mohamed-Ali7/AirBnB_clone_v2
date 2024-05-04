5d601ab26e6a8df5a5c89cc7c1bdd7ec7f8ac89a
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 13:33:21 +0300

    Making some updates for testing purposes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 # This script sets up a web servers for the deployment of web_static
 
-apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir /data/web_static/shared/
-echo "Hello World!" > /data/web_static/releases/test/index.html
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir /data/web_static/shared/
+sudo echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-
-service nginx restart
+sudo service nginx restart
