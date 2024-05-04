2df217b252396add8190686d22910728be635f62
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 05:37:33 +0300

    Adding 101-setup_web_static.pp file

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,32 @@
+# Puppet script to install and configure an Nginx server
+
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package { 'nginx':
+ensure          => installed,
+provider        => 'apt',
+install_options => ['-y'],
+}
+-> exec {'mkdir -p /data/web_static/releases/test/':
+path => '/usr/bin',
+}
+-> exec {'mkdir -p /data/web_static/shared/':
+path => '/usr/bin',
+}
+-> file {'/data/web_static/releases/test/index.html':
+content => 'Hello World!',
+}
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+}
+-> file_line { 'add_alias':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen \[::\]:80 default_server;',
+  line   => "\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}",
+}
+-> service { 'nginx':
+  ensure => 'running',
+}
