0ae61b5e65e2be8bbc1073d60bab72d054f99861
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 03:39:10 +0300

    Updating 2-do_deploy_web_static.py file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,21 +9,6 @@ import os
 
 env.hosts = ['54.208.120.231', '54.89.61.73']
 
-
-def do_pack():
-    """Generates a .tgz archive from the contents of the web_static"""
-
-    local("mkdir -p versions")
-
-    now = datetime.now()
-    archive_name = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
-
-    result = local(f"tar -czvf versions/{archive_name} web_static")
-
-    if result.succeeded:
-        return f"versions/{archive_name}"
-
-
 def do_deploy(archive_path):
     """Distributes an archive to a web servers"""
 
