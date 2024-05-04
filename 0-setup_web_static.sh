4df0ca4413858b34d04aff9e536469262a41adcf
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 13:25:19 +0300

    Making some updates for testing purposes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,6 +22,13 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+new="\n\
+\tlocation /hbnb_static {\n\
+\t\talias /data/web_static/current/;\n\
+\t}
+"
+sudo sed -i "/listen \[::\]:80 default_server;/a\ $new" /etc/nginx/sites-available/default
+
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
