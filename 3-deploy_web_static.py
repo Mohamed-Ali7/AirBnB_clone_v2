a9ce45392f7b4d2ac170600dc7e5a6b2cb69dbc8
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 14:11:23 +0300

    Making some updates for testing purposes

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -14,7 +14,7 @@ def do_pack():
     """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
+        if os.path.isdir("versions") is False:
             local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
@@ -25,7 +25,7 @@ def do_pack():
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if os.path.exists(archive_path) is False:
         return False
     try:
         file_n = archive_path.split("/")[-1]
