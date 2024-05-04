0ad63f626754b6696d438adf04ab964994944caa
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 04:19:34 +0300

    Updating 1-pack_web_static.py file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,13 +9,11 @@ from datetime import datetime
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static"""
 
-    local("mkdir -p versions")
-
-    now = datetime.now()
-    archive_name = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
-
-    result = local(f"tar -czvf versions/{archive_name} web_static")
-
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
     if result.succeeded:
-        return f"versions/{archive_name}"
-    return None
+        return filename
+    else:
+        return None
