1417c391a3ce17f9bba286e5a8ad363bb356ad0e
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 10:04:01 +0300

    Updating 101-setup_web_static.pp file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,36 +3,44 @@
 exec { 'update':
   command => '/usr/bin/apt-get update',
 }
--> package { 'nginx':
+->
+package { 'nginx':
 ensure          => installed,
 provider        => 'apt',
 install_options => ['-y'],
 }
--> exec {'create test directory':
+->
+exec {'create test directory':
   command  => 'mkdir -p /data/web_static/releases/test/',
   provider => 'shell',
 }
--> exec { 'create shared directory':
+->
+exec { 'create shared directory':
 command  => 'mkdir -p /data/web_static/shared/',
 provider => 'shell',
 }
--> file {'/data/web_static/releases/test/index.html':
+->
+file {'/data/web_static/releases/test/index.html':
 content => 'Hello World!',
 }
--> file { '/data/web_static/current':
+->
+file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test',
 }
--> exec { 'change owner':
+->
+exec { 'change owner':
 command  => 'chown -hR ubuntu:ubuntu /data/',
 provider => shell,
 }
--> file_line { 'add_alias':
+->
+file_line { 'add_alias':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen \[::\]:80 default_server;',
   line   => "\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}",
 }
--> service { 'nginx':
+->
+service { 'nginx':
   ensure => 'running',
 }
