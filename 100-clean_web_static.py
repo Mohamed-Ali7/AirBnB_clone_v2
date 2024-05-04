d3e6d1a97b410483284faef66a2a38f722645b78
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 07:30:16 +0300

    Updating 100-clean_web_static.py file

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,24 +11,22 @@ env.hosts = ['54.208.120.231', '54.89.61.73']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
+    """Deletes out-dated archives.
 
     Args:
         number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
     """
-    number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    number = int(number)
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    if number == 0:
+        number = 1
+
+    local_archives = local("ls -t versions", capture=True).split()
+    for archive in local_archives[number:]:
+        local(f"rm versions/{archive}")
+
+    remote_archives = run("ls -t /data/web_static/releases").split()
+    for archive in remote_archives[number:]:
+        if archive.startswith("web_static"):
+            run(f"rm -rf /data/web_static/releases/{archive}")
