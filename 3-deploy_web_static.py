e58b29beaeb20b120ddb924d6328a0e9bbaeedd8
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-04 12:48:16 +0300

    Updating 3-deploy_web_static.py file

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -29,23 +29,24 @@ def do_deploy(archive_path):
     try:
         if not os.path.exists(archive_path):
             return False
-        put(archive_path, "/tmp/")
 
         archive_name = archive_path.split("/")[-1]
-        web_path = f"/data/web_static/releases/{archive_name.split('.')[0]}"
+        web_path = "/data/web_static/releases/{}"\
+            .format(archive_name.split('.')[0])
 
-        run(f"mkdir -p {web_path}")
+        put(archive_path, "/tmp/")
 
-        run(f"tar -xzf /tmp/{archive_name} -C {web_path}")
-        run(f"rm /tmp/{archive_name}")
+        run("mkdir -p {}".format(web_path))
 
-        run(f"mv {web_path}/web_static/* {web_path}")
+        run("tar -xzf /tmp/{} -C {}/".format(archive_name, web_path))
+        run("rm /tmp/{}".format(archive_name))
 
-        run(f"rm -rf {web_path}/web_static")
+        run("mv {}/web_static/* {}".format(web_path, web_path))
 
-        run(f"rm -rf /data/web_static/current")
-        run(f"ln -s {web_path} /data/web_static/current")
+        run("rm -rf {}/web_static".format(web_path))
 
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {}/ /data/web_static/current".format(web_path))
         return True
     except:
         return False
