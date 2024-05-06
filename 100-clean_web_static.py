aedf51f588a221ecbb10985f9887d3534014a245
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-06 14:49:05 +0300

    Updating 100-clean_web_static.py file

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -17,17 +17,16 @@ def do_clean(number=0):
         number (int): The number of archives to keep.
     """
 
-    try:
-        number = int(number)
-        if number < 1:
-            number = 1
-        with cd('/data/web_static/releases'):
-            archives = run('ls -t').split()
-            for archive in archives[number:]:
-                run('rm -rf {}'.format(archive))
-        with lcd('versions'):
-            archives = local('ls -t', capture=True).split()
-            for archive in archives[number:]:
-                local('rm -rf {}'.format(archive))
-    except:
-        return False
+    number = int(number)
+
+    if number == 0:
+        number = 1
+
+    remote_archives = run("ls -t /data/web_static/releases/ | grep web_stat*")
+    remote_archives = remote_archives.split()
+    for archive in remote_archives[number:]:
+        sudo("rm -rf /data/web_static/releases/{}".format(archive))
+
+    local_archives = local("ls -t versions || true", capture=True).split()
+    for archive in local_archives[number:]:
+        local("rm -f versions/{}".format(archive))
