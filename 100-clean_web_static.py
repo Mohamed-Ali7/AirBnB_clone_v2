1ceba627c4d4cc4accb3efe69e6aeacda4a65924
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-06 14:54:55 +0300

    Updating 100-clean_web_static.py file

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,6 +7,8 @@ import os
 from fabric.api import *
 
 
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 env.hosts = ['54.208.120.231', '54.89.61.73']
 
 
@@ -22,11 +24,11 @@ def do_clean(number=0):
     if number == 0:
         number = 1
 
+    local_archives = local("ls -t versions || true", capture=True).split()
+    for archive in local_archives[number:]:
+        local("rm -f versions/{}".format(archive))
+
     remote_archives = run("ls -t /data/web_static/releases/ | grep web_stat*")
     remote_archives = remote_archives.split()
     for archive in remote_archives[number:]:
         sudo("rm -rf /data/web_static/releases/{}".format(archive))
-
-    local_archives = local("ls -t versions || true", capture=True).split()
-    for archive in local_archives[number:]:
-        local("rm -f versions/{}".format(archive))
