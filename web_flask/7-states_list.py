40306b2ea85396e258c8d919f58387afd546ed3b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-18 19:37:51 +0300

    Updating web_flask/7-states_list.py  file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,7 @@ Routes:
 from models import storage
 from flask import Flask
 from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
 
@@ -18,7 +19,7 @@ def states_list():
 
     States are sorted by name.
     """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
