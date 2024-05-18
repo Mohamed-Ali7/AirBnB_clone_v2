c0bf469bbccba35bc424705e9ab885dc89cf0ebc
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-18 16:43:46 +0300

    Updating setup_mysql_dev.sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,9 +8,9 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Gives the newly created user 'hbnb_dev' all privileges on the database hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Gives the newly created user 'hbnb_dev' SELECT privilege on the database performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
