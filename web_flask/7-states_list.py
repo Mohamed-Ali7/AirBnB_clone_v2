8cec5ee24276604daf3debe8ef83037856ddc442
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-18 19:37:23 +0300

    Updating web_flask/7-states_list.py  file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""This module starts a Flask web application on 0.0.0.0 on port 5000"""
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def list_of_states():
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
     """
-    displays a HTML page that contains a list of states
-    """
-    return render_template("7-states_list.html", states=storage.all(State))
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Executes after each request"""
-
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
